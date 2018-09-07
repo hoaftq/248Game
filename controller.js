32b78e604f084d07f8d167c8ab7374a0bda9dd59
hoaftq <hoaftq@gmail.com>
2018-09-08 02:20:38 +0700

    Complete major functions for computer version

--- controller.js
+++ controller.js
@@ -5,61 +5,83 @@ const X_SIZE = 4;
 const Y_SIZE = 4;
 
 function GameController() {
+    const direct = {
+        ArrowUp: 'up',
+        ArrowDown: 'down',
+        ArrowLeft: 'left',
+        ArrowRight: 'right'
+    };
+
+    var that = this;
+    var logic;
+    var view;
     this.bestScore = 0;
 
     this.newGame = function () {
-        var logic = new GameLogic(X_SIZE, Y_SIZE);
-        var view = new GameView(X_SIZE, Y_SIZE, 80, 10, 100);
+        initGame();
 
-        logic.initGameBoard();
-        view.init('#container');
-
+        // Create 2 new random tile
         for (let i = 0; i < 2; i++) {
             logic.putRandomTile(function (pos, text) {
                 view.putAt(pos, text);
             });
         }
 
-        $(document).keydown(function (e) {
-            var direct;
-            switch (e.key) {
-                case 'ArrowUp':
-                    direct = 'up';
-                    break;
+        // Register key handler for the game
+        $(document).off('keydown').keydown(function (e) {
 
-                case 'ArrowDown':
-                    direct = 'down';
-                    break;
-
-                case 'ArrowLeft':
-                    direct = 'left';
-                    break;
-
-                case 'ArrowRight':
-                    direct = 'right';
-                    break;
+            // If the key is not one of the arrows then ignore it
+            if (!direct[e.key]) {
+                return;
             }
 
-            if (direct) {
-                logic.move(
-                    direct,
-                    function (from, to, newText) {
-                        view.move(from, to, newText);
-                    },
-                    function (score, addedScore) {
-                        $('#score span').text(score);
-                    });
+            // Move tile according to the direction
+            logic.move(
+                direct[e.key],
 
-                setTimeout(function () {
-                    logic.putRandomTile(function (pos, text) {
-                        view.putAt(pos, text);
+                // Render movement on view
+                function (from, to, newText) {
+                    view.move(from, to, newText);
+                },
 
-                        if (logic.isGameOver()) {
-                            alert("Game over!");
-                        }
-                    });
-                }, 150);
-            }
+                // Render new score
+                function (score, addedScore) {
+                    $('#score').text(score);
+
+                    if (score > that.bestScore) {
+                        that.bestScore = score;
+                        $('#highest').text(that.bestScore);
+                    }
+                });
+
+            setTimeout(function () {
+                logic.putRandomTile(function (pos, text) {
+                    view.putAt(pos, text);
+                    if (logic.isGameOver()) {
+                        view.gameOver();
+                    }
+                });
+            }, 150);
+
         });
+    }
+
+    function initGame() {
+
+        // First time enter the page
+        if (!(logic && view)) {
+            logic = new GameLogic(X_SIZE, Y_SIZE);
+            view = new GameView(X_SIZE, Y_SIZE, 80, 10, 100);
+
+            logic.initGameBoard();
+            view.init('#container');
+        } else {
+            // Click New Game button
+            logic.clearGameBoard();
+            view.clear();
+        }
+
+        $('#score').text(0);
+        $('#highest').text(that.bestScore);
     }
 }
--- view.js
+++ view.js
@@ -5,7 +5,13 @@ function GameView(xDim, yDim, tileSize, padding, animationSpeed) {
     var size = tileSize + padding;
     var board;
 
+    /**
+     * Initialize a game board in the given container
+     * @param {*} container 
+     */
     this.init = function (container) {
+
+        // Create a game-board div that will contains all the tiles
         board = $('<div></div>')
             .addClass('game-board')
             .css({
@@ -14,12 +20,23 @@ function GameView(xDim, yDim, tileSize, padding, animationSpeed) {
             })
             .appendTo(container);
 
+        // Create backgound tiles
         for (let i = 0; i < xDim; i++) {
             for (let j = 0; j < yDim; j++) {
-                board.append(createTile({ x: i, y: j }, 'background-tile'));
+                board.append(createTile({ x: i, y: j }));
             }
         }
+    }
 
+    /**
+     * Clear the game board.
+     * After this function the game board is reset to the initialization state
+     */
+    this.clear = function () {
+
+        // Remove all tiles that is created when playing
+        board.find('.play-tile').remove();
+        $('#popup').remove();
     }
 
     /**
@@ -27,78 +44,100 @@ function GameView(xDim, yDim, tileSize, padding, animationSpeed) {
      * @param {*} pos 
      * @param {*} text 
      */
-    this.putAt = function (pos, text) {
-        createTile(pos, pos.x + '_' + pos.y, text, true).appendTo(board).fadeIn(animationSpeed);
+    this.putAt = function (pos, value) {
+
+        // Creates a hidden tile then shows it with fade-in animation
+        createTile(pos, 'play-tile ' + createPosClass(pos), value, true)
+            .appendTo(board)
+            .fadeIn(animationSpeed);
     }
 
     /**
-     * Move a tile from posFrom to posTo and set a new text to it if specifed.
-     * If there is another tile at posTo then this title will be removed first.
+     * Move a tile from posFrom to posTo and set a new value to it if specifed.
+     * If there is another tile at posTo then this tile will be removed first.
      * 
      * @param {*} posFrom 
      * @param {*} posTo 
-     * @param {*} newValue 
+     * @param {*} newValue optional
      */
     this.move = function (posFrom, posTo, newValue) {
+        var fromClass = createPosClass(posFrom);
+        var toClass = createPosClass(posTo);
 
         // If source position and destination position are different
         if (posFrom.x != posTo.x || posFrom.y != posTo.y) {
 
-            var fromClass = makePosClass(posFrom);
-            var toClass = makePosClass(posTo);
-
-            var fromTile = board
-                .find('.' + fromClass);
+            var fromTile = board.find('.' + fromClass);
             var toTile = board.find('.' + toClass);
 
-            // There is a tile at the same position as the destinaton then remove it first
+            // There is a tile at the same position as the destinaton then removes it first with animation
             toTile.fadeOut(animationSpeed, function () {
                 this.remove();
             });
 
             // Move the tile from posFrom to posTo with animation
             var tile = fromTile
-                .animate(getPosObj(posTo), animationSpeed)
+                .animate({
+                    left: (padding + posTo.x * size) + 'px',
+                    top: (padding + posTo.y * size) + 'px'
+                }, animationSpeed)
                 .removeClass(fromClass)
                 .addClass(toClass);
         } else {
-            var toClass = makePosClass(posTo);
             var tile = board.find('.' + toClass);
         }
 
         // A new value need to be applied to the destination tile
         if (newValue) {
             tile.queue(function () {
-                tile.text(newValue).addClass('v' + newValue).dequeue();
+                tile.text(newValue)
+                    .removeClass('v' + (newValue / 2))
+                    .addClass('v' + newValue)
+                    .dequeue();
             });
         }
     }
 
-    function makePosClass(pos) {
+    this.gameOver = function () {
+        $('<div>Game Over!</div>').attr('id', 'popup').css({
+            'margin-top': '-' + board.css('height'),
+            width: board.css('width'),
+            height: board.css('height'),
+            'line-height': board.css('height'),
+            opacity: 0.5,
+        }).insertAfter(board);
+    }
+
+    /**
+     * Create a class that contains position of a tile
+     * @param {*} pos 
+     */
+    function createPosClass(pos) {
         return pos.x + '_' + pos.y;
     }
 
-    function getPosObj(pos) {
-        return {
-            left: (padding + pos.x * size) + 'px',
-            top: (padding + pos.y * size) + 'px'
-        }
-    }
-
+    /**
+     * Create a new tile with given properties
+     * @param {*} pos 
+     * @param {*} clazz optional
+     * @param {*} value optional
+     * @param {*} hide optional
+     */
     function createTile(pos, clazz, value, hide) {
-        var tile = $('<div></div>')
-            .css({
-                left: (padding + pos.x * size) + 'px',
-                top: (padding + pos.y * size) + 'px',
-                width: tileSize + 'px',
-                height: tileSize + 'px',
-                'line-height': tileSize + 'px'
-            })
-            .addClass(clazz)
-            .addClass('v' + value);
+        var tile = $('<div></div>').css({
+            left: (padding + pos.x * size) + 'px',
+            top: (padding + pos.y * size) + 'px',
+            width: tileSize + 'px',
+            height: tileSize + 'px',
+            'line-height': tileSize + 'px'
+        });
+
+        if (clazz) {
+            tile.addClass(clazz)
+        }
 
         if (value) {
-            tile.text(value);
+            tile.text(value).addClass('v' + value);
         }
 
         if (hide) {
